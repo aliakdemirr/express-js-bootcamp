b393cba1601e79df95ea885c1f8d948347e16e39
ali.akdemir <ali.akdemir@konzek.com>
2022-08-22 00:09:46 +0300

    [master] Changed comment name and validation

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get('/developers', (req, res) => { // INDEX
 
 app.get('/developers/:id', (req, res) => { // SHOW
   const id = req.params.id
-  console.log('req.body ---->', req.body)
   const developer = seedData.find(dev => dev.id === parseInt(id))
   if (developer) {
     res.status(200).send(developer)
@@ -27,9 +26,9 @@ app.post('/developers', (req, res) => { // CREATE
   const newDeveloper = req.body
   if (newDeveloper.name === undefined || newDeveloper.name === null) {
     res.status(400).send('Developer name is required')
-  } else if (!newDeveloper.skills) {
+  } else if (newDeveloper.skills.length < 0) {
     res.status(400).send('Developer skills is required')
-  } else if (!Array.isArray(newDeveloper.skills) || newDeveloper.skills.length < 0) {
+  } else if (!Array.isArray(newDeveloper.skills)) {
     res.status(400).send('Developer skills should be an array and not be empty')
   } else if (!newDeveloper.title) {
     res.status(400).send('Developer title is required')
@@ -46,15 +45,15 @@ app.post('/developers', (req, res) => { // CREATE
   }
 })
 
-app.put('/developers/:id', (req, res) => { // CREATE
+app.put('/developers/:id', (req, res) => { // REPLACE
   const replacedDeveloperId = req.params.id
   const replacedDeveloper = seedData.find(dev => dev.id === parseInt(replacedDeveloperId))
   if (replacedDeveloper && replacedDeveloperId) {
     if (!req.body.name) {
       res.status(400).send('Name parameter is required!!')
-    } else if (!req.body.skills) {
+    } else if (req.body.skills.length < 0) {
       res.status(400).send('Skills parameter is required!!')
-    } else if (!Array.isArray(req.body.skills) || req.body.skills.length < 0) {
+    } else if (!Array.isArray(req.body.skills)) {
       res.status(400).send('Skills parameter should be an array or length of not less than zero!!')
     } else if (!req.body.salary) {
       res.status(400).send('Salary parameter is required!!')
@@ -69,7 +68,6 @@ app.put('/developers/:id', (req, res) => { // CREATE
       replacedDeveloper.title = req.body.title
       res.status(200).send(replacedDeveloper)
     }
-
   } else {
     res.status(404).send("developer not found")
   }
